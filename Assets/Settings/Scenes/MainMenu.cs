205186f7552bb6ed09b979907daf67a339e039e0
RHuangcode <ryan.huang26@ycdsbk12.ca>
2025-05-27 20:27:12 -0400

    MainMenu Scenes and Play Button
    - Added scenes for main menu, game, preferences and settings
    - Button for play created
    - Code for play button added in script
    - Button function still needs to be connected to play button

---
+++ Assets/Settings/Scenes/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

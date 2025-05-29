005106e2fbbff54d117c558a6992b85fb5b02108
al-1108 <altyw69420@gmail.com>
2025-05-29 10:43:10 -0400

    Updated Entity.cs
    - Created getters and setters for health and damage
    - Added max health variable
    - created TakeDamage and Heal functions

--- Assets/scripts/Entity.cs
+++ Assets/scripts/Entity.cs
@@ -1,7 +1,45 @@
 public class Entity
 {
-    //encapsulate the child classes
-    public int Health;
-    public int Damage;
-    public float Speed;
+    private int _health;
+    private int _maxHealth;
+    private int _damage;
+    private float _speed;
+
+    public int GetHealth()
+    {
+        return _health;
+    }
+
+    public void SetHealth(int health)
+    {
+        _health = health;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _health -= damage;
+        if (_health < 0)
+        {
+            _health = 0;
+        }
+    }
+
+    public void Heal(int heal)
+    {
+        _health += heal;
+        if (_health > _maxHealth)
+        {
+            _health = _maxHealth;
+        }
+    }
+
+    public int GetDamage()
+    {
+        return _damage;
+    }
+
+    public void SetDamage(int damage)
+    {
+        _damage = damage;
+    }
 }

f7bc28746ee9108fc5832170a4bfbc9b94898740
RHuangcode <ryan.huang26@ycdsbk12.ca>
2025-05-31 23:23:04 -0400

    Added save user progress Function and Coin Class
    - Added a quit game function in main menu which saves user progress to json file, which is currently only coins
    
    - Added a new Coin class with a year and a money value

---
+++ Assets/scripts/Coin.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Coin
+{
+    public int value;
+    public int year;
+
+    // Constructor
+    public Coin(int value, int year)
+    {
+        this.value = value;
+        this.year = year;
+    }
+}
--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -1,8 +1,11 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class MainMenu : MonoBehaviour
 {
+    public List<Coin> coins = new List<Coin>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,9 +20,26 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("PreferencesScene");
     }
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        DataWrapper wrapper = new DataWrapper(coins);
+
+        string json = JsonUtility.ToJson(wrapper, true);
+
+        string savePath = Path.Combine(Application.persistentDataPath, "GameProgress.json");
+
+        File.WriteAllText(savePath, json);
+
+        Application.Quit();
     }
+    public class DataWrapper
+    {
+        public List<Coin> coins;
+
+        public DataWrapper(List<Coin> coins)
+        {
+            this.coins = coins;
+        }
+    }
+
 }

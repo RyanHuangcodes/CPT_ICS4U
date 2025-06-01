0a831494a40896d8f27ec6a02c74cde6cf9b82b2
al-1108 <altyw69420@gmail.com>
2025-06-01 19:59:44 -0400

    Updated PlayerMovement.cs
    - missed changes from previous commit

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float _moveSpeed = 10f;
+    private Rigidbody2D myBody; 
+    private float _movementX;
+    private float _movementY;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {

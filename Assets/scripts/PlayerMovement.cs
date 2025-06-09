df1c51cba4507e1681e9fa17b636085e66d4c59a
dvergeldedios <159474149+dvergeldedios@users.noreply.github.com>
2025-06-09 17:06:01 -0400

    Complete Animations, Idle, Running

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -7,8 +7,17 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D _rb;
     private Vector2 _moveDirection;
 
-    void Start() {
+    private Animator _anim;
+    private SpriteRenderer _spriteRenderer;
+
+    // Track last horizontal direction for flipping
+    private bool facingRight = true;
+
+    void Start()
+    {
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -16,12 +25,28 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         _moveDirection = new Vector2(moveX, moveY).normalized;
-        // remove rotation
-        transform.rotation = Quaternion.identity;
 
+        bool isRunning = _moveDirection.magnitude > 0;
+        _anim.SetBool("isRunning", isRunning);
+
+        // Only change flip if moving left or right
+        if (moveX > 0)
+        {
+            facingRight = true;
+            _spriteRenderer.flipX = false;
+        }
+        else if (moveX < 0)
+        {
+            facingRight = false;
+            _spriteRenderer.flipX = true;
+        }
+        // If moving only vertically, keep facing last horizontal direction (do nothing)
+
+        transform.rotation = Quaternion.identity;
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         _rb.linearVelocity = _moveDirection * MoveSpeed;
     }
 }

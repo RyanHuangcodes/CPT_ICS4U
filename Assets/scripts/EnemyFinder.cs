8109985291639552d28b663629612449562c4ec4
al-1108 <altyw69420@gmail.com>
2025-06-12 09:26:59 -0400

    updated enemyfinder

--- Assets/scripts/EnemyFinder.cs
+++ Assets/scripts/EnemyFinder.cs
@@ -31,7 +31,7 @@ public static class EnemyFinder
 
         return closest;
     }
-    public static Enemy FindClosestEnemyToBaseInRange(float range)
+    public static Enemy FindClosestEnemyToBaseInRange(Vector2 towerPos, float towerRange)
     {
         GameObject baseObj = GameObject.FindWithTag("Base");
         if (baseObj == null)
@@ -40,22 +40,17 @@ public static class EnemyFinder
         }
 
         Vector2 basePos = baseObj.transform.position;
-        float rangeSq = range * range;
-
-        Enemy[] all = Object.FindObjectsByType<Enemy>(
+        Enemy[] all = Object.FindObjectsOfType<Enemy>(
             FindObjectsInactive.Exclude,
             FindObjectsSortMode.None
         );
 
+        float rangeSq = towerRange * towerRange;
         int count = 0;
         for (int i = 0; i < all.Length; i++)
         {
-            Vector2 pos = all[i].transform.position;
-            float distancex = pos.x - basePos.x;
-            float distancey = pos.y - basePos.y;
-            float distSq = distancex * distancex + distancey * distancey;
-
-            if (distSq <= rangeSq)
+            float dsq = ((Vector2)all[i].transform.position - towerPos).sqrMagnitude;
+            if (dsq <= rangeSq)
             {
                 all[count] = all[i];
                 count++;
@@ -74,85 +69,80 @@ public static class EnemyFinder
             arr[i] = all[i];
         }
 
-        void MergeSort(int left, int right)
-        {
-            if (left >= right)
-            {
-                return;
-            }
-
-            int mid = left + (right - left) / 2;
-
-            MergeSort(left, mid);
-            MergeSort(mid + 1, right);
-
-            int i = left;
-            int j = mid + 1;
-            int k = left;
-
-            while (i <= mid && j <= right)
-            {
-                float di = ((Vector2)arr[i].transform.position - basePos).sqrMagnitude;
-                float dj = ((Vector2)arr[j].transform.position - basePos).sqrMagnitude;
-
-                if (di <= dj)
-                {
-                    tmp[k] = arr[i];
-                    k++;
-                    i++;
-                }
-                else
-                {
-                    tmp[k] = arr[j];
-                    k++;
-                    j++;
-                }
-            }
-
-            while (i <= mid)
-            {
-                tmp[k] = arr[i];
-                k++;
-                i++;
-            }
-
-            while (j <= right)
-            {
-                tmp[k] = arr[j];
-                k++;
-                j++;
-            }
-
-            for (int t = left; t <= right; t++)
-            {
-                arr[t] = tmp[t];
-            }
-        }
-
-        MergeSort(0, count - 1);
+        MergeSort(arr, tmp, 0, count - 1, basePos);
         return arr[0];
     }
+
+    private static void MergeSort(Enemy[] arr, Enemy[] tmp, int left, int right, Vector2 basePos)
+    {
+        if (left >= right)
+        {
+            return;
+        }
+
+        int mid = left + (right - left) / 2;
+        MergeSort(arr, tmp, left, mid, basePos);
+        MergeSort(arr, tmp, mid + 1, right, basePos);
+
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while (i <= mid && j <= right)
+        {
+            float di = ((Vector2)arr[i].transform.position - basePos).sqrMagnitude;
+            float dj = ((Vector2)arr[j].transform.position - basePos).sqrMagnitude;
+            if (di <= dj)
+            {
+                tmp[k] = arr[i];
+                i++;
+            }
+            else
+            {
+                tmp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= mid)
+        {
+            tmp[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j <= right)
+        {
+            tmp[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for (int t = left; t <= right; t++)
+        {
+            arr[t] = tmp[t];
+        }
+    }
+
     public static Enemy FindHighestHealthTarget(Vector2 origin, float range)
     {
         float rangeSquared = range * range;
 
-        Enemy[] allEnemies = Object.FindObjectsByType<Enemy>(
-            FindObjectsInactive.Exclude,
-            FindObjectsSortMode.None
-        );
+        Enemy[] allEnemies = Object.FindObjectsByType<Enemy>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
 
         Enemy highestHealthEnemy = null;
         float highestHealth = float.MinValue;
 
-        for (int i = 0; i < allEnemies.Length; i++) // Linear search through all the enemies for highest health
+        for (int i = 0; i < allEnemies.Length; i++) 
         {
-            Enemy enemy = allEnemies[i]; // current enemy that it looks through
-            Vector2 position = enemy.transform.position; // gets position of enemy
+            Enemy enemy = allEnemies[i]; 
+            Vector2 position = enemy.transform.position; 
             float distanceX = position.x - origin.x;
             float distanceY = position.y - origin.y;
-            float distanceSquared = distanceX * distanceX + distanceY * distanceY; //distance
+            float distanceSquared = distanceX * distanceX + distanceY * distanceY; 
 
-            if (distanceSquared <= rangeSquared) // only consider enemies within range
+            if (distanceSquared <= rangeSquared) 
             {
                 float health = enemy.GetHealth(); 
                 if (health > highestHealth)
